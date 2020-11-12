e77c2d403fa89df8e4f26cc931806f1d91449cca
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-12 13:06:04 -0800

    feat(server): added client func

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const io = require('socket.io')();
-const PORT = 3000
+const PORT = 8080
+
+const { handleJoinGame, handleNewGame, removePlayer } = require('./functions')
 
 io.on('connection', client => {
-  console.log('someone connected or something like that idk')
-  client.emit('init', { data: 'hello world' })
+  console.log('CONNECTED')
+  client.emit('init', { data: 'CONNECTED', id: client.id })
+
+
+  client.on('joinGame', (roomName) => handleJoinGame(io, client, roomName));
+  client.on('hostGame', () => handleNewGame(client));
+    
+
+  // client.join('GAME')
+  // console.log(io.sockets.adapter.rooms['GAME'], 'ROOMS BOI BOIIII')
+  client.on('disconnect', () => {
+    removePlayer(io, Object.keys(client.adapter.rooms)[0], client.name)
+  });
 });
 
 console.log(`Listening on ${process.env.PORT || PORT}`)
