f571fcc0ae2528190a5e9fda97c53a0d9ec0a9f7
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-28 11:49:25 -0800

    feat(functions): handle user making move logic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const io = require('socket.io')();
 const PORT = 8080
 
-const { handleHostLeaveGame, handleJoinGame, handleNewGame, removePlayer, refreshLobbyPlayers, handleHostStartsGame, getPlayerOpponent } = require('./functions')
+const { handleHostLeaveGame, handleJoinGame, handleNewGame, handlePlayerMove, removePlayer, refreshLobbyPlayers, handleHostStartsGame, getPlayerOpponent } = require('./functions')
 
 io.on('connection', client => {
   console.log('CONNECTED')
@@ -15,6 +15,18 @@ io.on('connection', client => {
   client.on('hostStartedGame', () => handleHostStartsGame(io, client))
   client.on('refreshLobbyPlayers', () => refreshLobbyPlayers(io, client.currentRoomName))
   client.on('findPlayerOpponent', () => getPlayerOpponent(client))
+
+
+  // Game Stuff
+  client.on('sendCurrentChoice', (choice) => {
+    if(client.hasUpdated) {
+      client.hasUpdated = false
+    } else {
+      handlePlayerMove(client, choice)
+      client.hasUpdated = true
+    } 
+  }
+  )
 
   client.on('disconnect', () => {
     removePlayer(io, client)
