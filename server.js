92ee24fa1b4edf916793dfe8be8d48f145b2c48a
Fredodiaz <alfredo.diazavina@gmail.com>
2020-11-30 12:56:27 -0800

    feat(server): added new endpoints)

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
 const io = require('socket.io')();
 const PORT = 8080
 
-const { handleHostLeaveGame, handleJoinGame, handleNewGame, handlePlayerMove, removePlayer, refreshLobbyPlayers, handleHostStartsGame, getPlayerOpponent } = require('./functions')
+const { handleHostLeaveGame, handleJoinGame, handleNewGame, handlePlayerMove, removePlayer, refreshLobbyPlayers, handleHostStartsGame, getPlayerOpponent, handleEachMatch } = require('./functions')
 
 io.on('connection', client => {
   console.log('CONNECTED')
   client.emit('init', { data: 'CONNECTED', id: client.id })
 
 
+  // LOBBY JOIN/LEAVE/HOST ENDPOINTS
   client.on('joinGame', (roomName) => handleJoinGame(io, client, roomName));
-  client.on('hostGame', () => handleNewGame(client));
+  client.on('hostGame', () => handleNewGame(client)); //
   client.on('leaveLobby', () => removePlayer(io, client));
-  client.on('hostLeaveGame', () => handleHostLeaveGame(io, client))
-  client.on('hostStartedGame', () => handleHostStartsGame(io, client))
-  client.on('refreshLobbyPlayers', () => refreshLobbyPlayers(io, client.currentRoomName))
-  client.on('findPlayerOpponent', () => getPlayerOpponent(client))
+  client.on('hostLeaveGame', () => handleHostLeaveGame(io, client)) //
+  client.on('hostStartedGame', () => handleHostStartsGame(io, client)) //
+  client.on('refreshLobbyPlayers', () => refreshLobbyPlayers(io, client.currentRoomName)) //
 
 
-  // Game Stuff
-  client.on('sendCurrentChoice', (choice) => {
+
+  // MISC GAME ENDPOINTS
+  client.on('findPlayerOpponent', () => {
+    console.log('HE WWOOOOOOOOOOOON', client.winner)
+    if(!client.winner) {
+      getPlayerOpponent(client)
+    }
+  }) // returns opponent object
+
+
+  // GAME ACTION ENDPOINTS
+  client.on('sendCurrentChoice', (choice) => { // gets move sent by client
     if(client.hasUpdated) {
       client.hasUpdated = false
     } else {
-      handlePlayerMove(client, choice)
+      // console.log(client.name, 'did one move')
+      handlePlayerMove(io, client, choice)
       client.hasUpdated = true
     } 
   }
   )
+
+  client.on('matchedAndReadyToBattle', () => handleEachMatch(io, client))
+
+  client.on('resetTheTimer', () => client.emit('startTimer'))
+
+
+
+
 
   client.on('disconnect', () => {
     removePlayer(io, client)
